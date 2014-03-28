ddcf38a3b2b6f2dd80c07eb302783ec01882a3e6
edward <edwardhornig@gmail.com>
2014-03-28 19:50:30 -0400

    Initialize repository

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+ruby '2.1.0'
+#ruby-gemset=jhad_rails_4_0
+
+gem 'rails', '4.0.4'
+
+group :development do
+	gem 'sqlite3'
+end
+
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jhad::Application.config.secret_key_base = 'e7a4f681b72e89617d4ce946ed07d86cfddc124c3b38bf696cc1400179d5979a75b71653fb0e094a3b5b034030ffb8075bc3c1f066effd72ec414ca7cc24c7c6'
