0958e9da1a7b4153563e62fcbd13c6ad8a0be014
edward <edwardhornig@gmail.com>
2014-03-29 13:29:39 -0400

    added named paths, layout links, added to tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,30 +7,44 @@ describe "StaticPages" do
 	let(:base_title) { "Just Had a Dream" }
 
 	describe "Home page" do
-		before { visit 'static_pages/home' }
+		before { visit root_path }
 
 		it { should have_title("#{base_title}") }
 		it { should have_content('home page') }
 	end
 
 	describe "Help page" do
-		before { visit 'static_pages/help' }
+		before { visit help_path }
 
 		it { should have_title("#{base_title} | Help") }
 		it { should have_content('help') }
 	end
 
 	describe "About page" do
-		before { visit 'static_pages/about' }
+		before { visit about_path }
 
 		it { should have_title("#{base_title} | About") }
 		it { should have_content('About') }
 	end
 
 	describe "Contact page" do
-		before { visit 'static_pages/contact' }
+		before { visit contact_path }
 
 		it { should have_title("#{base_title} | Contact") }
 		it { should have_content('Contact') }
+	end
+
+	it "should have the right links in the layout" do
+		visit root_path
+		click_link "Help"
+		expect(page).to have_title("#{base_title} | Help")
+		click_link "About"
+		expect(page).to have_title("#{base_title} | About")
+		click_link "Home"
+		expect(page).to have_title("#{base_title}")
+		click_link "Contact"
+		expect(page).to have_title("#{base_title} | Contact")
+		click_link "Just Had a Dream"
+		expect(page).to have_title("#{base_title}")
 	end
 end
