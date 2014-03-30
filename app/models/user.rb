594d4af09f321cc72fb821164e9e908fbeae55f7
edward <edwardhornig@gmail.com>
2014-03-30 16:46:32 -0400

    Add username table to User model. Change so user can login and do other actions with either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+  		where(conditions).where(["lower(username) = :value OR lower(email) = :value", 
+  														{ :value => login.downcase }]).first 
+  	else
+  		where(conditions).first
+  	end
+  end
+
+  # This is the correct method you override with the code above
+  # def self.find_for_database_authentication(warden_conditions)
+  # end
 end
