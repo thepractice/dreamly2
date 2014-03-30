65e7d9e53f4f640b036dbc36a37415c5c7d9ad3a
edward <edwardhornig@gmail.com>
2014-03-30 12:15:33 -0400

    Change devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Jhad::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
   root 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
