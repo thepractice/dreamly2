2647300692d1ec4dcad8305f937667723b17f54a
edward <edwardhornig@gmail.com>
2014-04-02 19:24:54 -0400

    Change environmental variables for omniauth to use bashrc

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, AUTH_CONFIG["twitter_consumer_key"], AUTH_CONFIG["twitter_secret_key"]
+  config.omniauth :twitter, ENV["twitter_consumer_key"], ENV["twitter_secret_key"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
