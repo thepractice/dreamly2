66e9f6d3f02df93389b272695ee476665ef42627
edward <edwardhornig@gmail.com>
2014-04-08 23:24:57 -0400

    update dream dates and date-picker

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,5 +1,6 @@
 class Dream < ActiveRecord::Base
 	belongs_to :user
+	default_scope -> { order('dreamed_on DESC') }
 	validates :body, presence: true
 	validates :user_id, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,12 +10,16 @@ describe User do
 
   describe "dream associations" do
   	before { user.save }
-  	let!(:first_dream) do
-  		FactoryGirl.create(:dream, user: user, created_at: 1.day.ago)
+  	let!(:older_dream) do
+  		FactoryGirl.create(:dream, user: user, created_at: 3.day.ago, dreamed_on: 3.day.ago)
   	end
-  	let!(:second_dream) do
-  		FactoryGirl.create(:dream, user: user, created_at: 1.hour.ago)
+  	let!(:newer_dream) do
+  		FactoryGirl.create(:dream, user: user, created_at: 1.hour.ago, dreamed_on: 1.hour.ago)
   	end
+
+    it "should have the right dreams in the right order" do
+      expect(user.dreams.to_a).to eq [newer_dream, older_dream]
+    end
 
   	it "should destroy associated dreams" do
   		dreams = user.dreams.to_a
