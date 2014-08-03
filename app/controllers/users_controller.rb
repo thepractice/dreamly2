634e43af3c02edaa6c782943fcf401435854549e
thepractice <edwardhornig@gmail.com>
2014-08-03 17:24:27 -0400

    style user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
 	def index
-		@users = User.paginate(page: params[:page], per_page: 15)
+#		@users = User.paginate(page: params[:page], per_page: 40)
+		@users = User.all
 	end
 
 	def show
