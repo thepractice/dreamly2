2d3cde277d94c79d3db48027e2932d41f3797bb1
thepractice <edwardhornig@gmail.com>
2014-08-05 20:10:34 -0400

    tweak for google oauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,20 +15,21 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		end
 	end
 
-  def google_oauth2
+#  def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+ #   @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-	end
+#    if @user.persisted?
+#      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+#      sign_in_and_redirect @user, :event => :authentication
+#    else
+#      session["devise.google_data"] = request.env["omniauth.auth"]
+#      redirect_to new_user_registration_url
+#    end
+#	end
 
 	alias_method :twitter, :all 		# Use the :all method when trying to use :twitter method
 	alias_method :facebook, :all
+	alias_method :google_oath2, :all
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -62,9 +62,9 @@ class User < ActiveRecord::Base
   end
 
   # for Google oath2
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
+ # def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+ #   data = access_token.info
+ #   user = User.where(:email => data["email"]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
     # unless user
@@ -73,8 +73,8 @@ class User < ActiveRecord::Base
     #        password: Devise.friendly_token[0,20]
     #     )
     # end
-    user
-  end
+#    user
+#  end
 
 
   # Override method on user model so that users using OmniAuth don't require password.
