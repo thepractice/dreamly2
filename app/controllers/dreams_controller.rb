92b2e987cecb9731e5c4fde663986a1a755e0c68
thepractice <edwardhornig@gmail.com>
2014-09-25 19:38:45 -0400

    finish some styling

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -42,8 +42,16 @@ class DreamsController < ApplicationController
 		@word_count_sort = @word_count_sort.sort_by { |k, v| v }
 		@word_count_sort.reverse!
 
+		if params[:graph]
+			@min_words_constant = 15
+			@min_assocs_constant = 2
+		else
+			@min_words_constant = 5
+			@min_assocs_constant = 2
+		end		
+
 		@nodes = Array.new
-		@min_words = [@word_count.length, 5].min
+		@min_words = [@word_count.length, @min_words_constant].min
 		@min_words.times do |n|
 			@nodes.push(@word_count_sort[n][0])			#@word_count_sort has become an array.
 		end
@@ -64,7 +72,7 @@ class DreamsController < ApplicationController
 			end
 
 			@word_object_assocs = Hash[@word_object_assocs.sort_by{|k, v| v}.reverse]
-			@min_assocs = [@word_object_assocs.length, 2].min
+			@min_assocs = [@word_object_assocs.length, @min_assocs_constant].min
 			@min_assocs.times do |i|
 				if @nodes.include? @word_object_assocs.keys[i]		# if the association is already a node
 					@links.push([@nodes.index(@word_object_id), @nodes.index(@word_object_assocs.keys[i])])	# write the link
@@ -116,8 +124,14 @@ class DreamsController < ApplicationController
 		@word_count_sort = @word_count.sort_by { |k, v| v }
 		@word_count_sort.reverse!
 
+		if params[:graph]
+			@min_words_constant = 15
+		else
+			@min_words_constant = 5
+		end				
+
 		@nodes = Array.new
-		@min_words = [@word_count.length, 5].min
+		@min_words = [@word_count.length, @min_words_constant].min
 		@min_words.times do |n|
 			@nodes.push(@word_count.keys[n])
 		end
