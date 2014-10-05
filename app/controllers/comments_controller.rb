e80bec1a70ac22938f854c6b006695fe3c6eae25
thepractice <edwardhornig@gmail.com>
2014-10-05 19:12:48 -0400

    work on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,18 @@ class CommentsController < ApplicationController
 		redirect_to dream_path(@dream)
 	end
 
+	def edit
+		@dream = Dream.find(params[:dream_id])
+		@comment = @dream.comments.find(params[:id])
+	end
+
+	def destroy
+		@dream = Dream.find(params[:dream_id])
+		@comment = @dream.comments.find(params[:id])
+		@comment.destroy
+		redirect_to dream_path(@dream)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:body, :user_id)
