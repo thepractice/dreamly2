5088aae12985a018025398adf08219c0d3fbb50a
thepractice <edwardhornig@gmail.com>
2015-03-20 23:01:35 -0400

    change root to dreams index, change dream counter

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Jhad::Application.routes.draw do
 
   
 
-  authenticated :user do
+#  authenticated :user do
     # Rails 4 users must specify the 'as' option to give it a unique name
-    root :to => "static_pages#home", :as => "authenticated_root"
-  end 
+#    root :to => "static_pages#home", :as => "authenticated_root"
+#  end 
 
-  unauthenticated do
-    root 'static_pages#home'
-  end
+#  unauthenticated do
+#    root 'static_pages#home'
+#  end
+
+  root 'dreams#index'
 
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout" },
                      controllers: { omniauth_callbacks: "omniauth_callbacks", sessions: 'sessions', registrations: 'registrations' }
@@ -31,6 +33,7 @@ Jhad::Application.routes.draw do
   resources :screennames
   resources :relationships, only: [:create, :destroy]
 
+  match '/feed',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
