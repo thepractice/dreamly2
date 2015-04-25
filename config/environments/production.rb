23515f2564582f64bff28ee13fbe94d793d15dd4
thepractice <edwardhornig@gmail.com>
2015-04-25 14:01:47 -0400

    tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Jhad::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # Changed!
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
