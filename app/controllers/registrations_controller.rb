762346b37e1f10f72ccda4585059a6c2c01862ed
thepractice <edwardhornig@gmail.com>
2015-04-26 15:51:09 -0400

    change registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -32,16 +32,27 @@ class RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        if current_user == nil
+          return render :json => {:success => true}
+        else
+          return render :json => {:success => true, :current_user_id => current_user.id}
+        end
+        #respond_with resource, location: after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        if current_user == nil
+          return render :json => {:success => true}
+        else
+          return render :json => {:success => true, :current_user_id => current_user.id}
+        end      
+        #respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      return render :json => {:success => false, :errors => resource.errors.full_messages}
+      #set_minimum_password_length
+      #respond_with resource
     end
   end
 
