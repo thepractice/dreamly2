16ba8497fb26111509c31e3ab519958967055b9e
thepractice <edwardhornig@gmail.com>
2015-04-26 14:53:01 -0400

    add confirmable to users migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417013841) do
+ActiveRecord::Schema.define(version: 20150426183830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,8 +211,13 @@ ActiveRecord::Schema.define(version: 20150417013841) do
     t.text     "hash_freq_public"
     t.text     "emotion_freq"
     t.text     "emotion_freq_public"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["dream_count"], name: "index_users_on_dream_count", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
