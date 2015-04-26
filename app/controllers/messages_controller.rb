818b0c6fd5f2ff5dcead730209ce364ad066ca6f
thepractice <edwardhornig@gmail.com>
2015-04-26 18:24:09 -0400

    tweak messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,7 +19,7 @@ class MessagesController < ApplicationController
 			flash[:success] = "Message has been sent!"
 			redirect_to conversation_path(@dialog)
 		end
-		user2.notify(user2, 'notification')
+		user2.notify('notify', 'notification')
 		
 	end
 
