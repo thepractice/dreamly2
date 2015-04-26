5f25cbbcfccee9695a1d53947774b6ff7928cfc9
thepractice <edwardhornig@gmail.com>
2015-04-26 15:44:00 -0400

    change registrations controller #create to default

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,49 @@
 class RegistrationsController < Devise::RegistrationsController
 
+#  def create
+#    build_resource(sign_up_params)
+
+#    resource_saved = resource.save
+#    yield resource if block_given?
+#    if resource_saved
+#      if resource.active_for_authentication?
+#        set_flash_message :notice, :signed_up if is_flashing_format?
+#        sign_up(resource_name, resource)
+#        return render :json => {:success => true, :current_user_id => current_user.id}
+#      else
+#        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+#        expire_data_after_sign_in!
+#        return render :json => {:success => true, :current_user_id => current_user.id}
+#      end
+#    else
+#      clean_up_passwords resource
+#      return render :json => {:success => false, :errors => resource.errors.full_messages}
+#    end
+#  end
+
+
+  # POST /resource
   def create
     build_resource(sign_up_params)
 
-    resource_saved = resource.save
+    resource.save
     yield resource if block_given?
-    if resource_saved
+    if resource.persisted?
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        return render :json => {:success => true, :current_user_id => current_user.id}
+        respond_with resource, location: after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        return render :json => {:success => true, :current_user_id => current_user.id}
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
-      return render :json => {:success => false, :errors => resource.errors.full_messages}
+      set_minimum_password_length
+      respond_with resource
     end
   end
+
 
 end
